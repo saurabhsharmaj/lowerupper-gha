f1cd69082402cb3b591099e8df69a7e7388bc3e4
saurabhsharmaj <saurabh.find@gmail.com>
2024-08-15 16:22:49 +0530

    Add files via upload

---
+++ src/main/java/com/example/lowerupper_gha/LowerupperGhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.lowerupper_gha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowerupperGhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowerupperGhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lowerupper_gha/LowerupperGhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lowerupper_gha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowerupperGhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
